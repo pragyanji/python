8a496827014ee807bba3ba02218187b937bbe597
pragyanji <pragyanpandey2058@gmail.com>
2024-05-21 07:41:39 +0545

    fourth

---
+++ guess_number.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+random_num = np.random.randint(0,100)
+count = 0
+while True:
+        guess =  input('Guess the number:')
+        if guess.isdigit():
+            count+=1
+            num = int(guess)
+            if num == random_num:
+                print(f'You guessed it right! its {random_num}')
+                print(f'You guessed the right number in {count}th attempt')
+                break
+            else:
+                print('Wrong guess! Try again')
+                if num < random_num:
+                    print('your guess is lower')
+                if num > random_num:
+                    print('your guess is higher')
+        else:
+            print('wrong input')
--- new.py
+++ new.py
@@ -1,8 +1,9 @@
-def rev_name(name):
-    return name[::-1]
-
-print(rev_name('pragyan'))
-def number():
-    return "hello"
-
-number()
+lst1 = [1,4,6,8,3,9,12,45]
+def even_sum(lst):
+    summ = 0
+    for i in lst:
+        if i % 2 == 0:
+            summ += i
+    return summ
+       
+print(f'sum of even number:{even_sum(lst1)}')
